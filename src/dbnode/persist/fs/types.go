87273003b889e8d87cae95b3cee81486416ef31f
Ryan Hall <ryanhall07@gmail.com>
2020-11-23 16:40:25 -0800

    Revert "[dbnode] Properly rebuild index segments if they fail verification. (#2879)" (#2944)
    This reverts commit c6fe28d2a95a830c47aad8e712c62a45a9ae6eb1.

--- src/dbnode/persist/fs/types.go
+++ src/dbnode/persist/fs/types.go
@@ -700,6 +700,3 @@ type IndexClaimsManager interface {
 		blockStart time.Time,
 	) (int, error)
 }
-
-// DeleteFilesFn deletes files passed in as arg.
-type DeleteFilesFn func(files []string) error
